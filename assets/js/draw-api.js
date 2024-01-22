fe88bec9a9f0a5a3294d870b113567b957bdb9b9
Anthonyzok521 <anthonyzok521@gmail.com>
2024-01-21 22:02:06 -0400

    Add styles and animations

---
+++ assets/js/draw-api.js
@@ -0,0 +1,8 @@
+/** @type {HTMLCanvasElement} */ //Habilitando autocompletado para el canvas
+const canvas = document.getElementsById('main-canvas'); //Canvas
+const context = canvas.getContext('2d');                    //Contexto en 2D
+const header = document.getElementsByTagName('header');
+
+const slideHeader = (evt) => {
+    
+}
