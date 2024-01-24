81031819a7e8f85be3c12ebbeeeaf80334e722d9
Anthonyzok521 <anthonyzok521@gmail.com>
2024-01-24 11:25:32 -0400

    Set canvas size using a callback with parent tag main

--- assets/js/draw-api.js
+++ assets/js/draw-api.js
@@ -1,13 +1,40 @@
 /** @type {HTMLCanvasElement} */ //Habilitando autocompletado para el canvas
-const canvas = document.getElementsById('main-canvas'); //Canvas
+const main = document.querySelector('main');
+const canvas = document.querySelector('#main-canvas'); //Canvas
 const context = canvas.getContext('2d');                    //Contexto en 2D
+//const btn
 
 //Coordenadas el mouses iniciales
 let iX, iY; //abreviacion de initialX, etc.
+
+//Función callback para redimencionar el canvas cada 200ms con respecto al tamaño del main
+const resize = (func) => {
+    let timer;
+    return (event) => {
+      if (timer) {
+        clearTimeout(timer)
+      }
+      timer = setTimeout(func, 200, event);
+    }
+}
+
+window.addEventListener('resize', resize(() => { 
+    canvas.width = window.innerWidth;
+    canvas.height = window.innerHeight;
+}));
 
 //Funciones flecha que usa eventos como parámetro
 //Dibujar
 
 const draw = (evt) =>{
-    
+    context.beginPath(); // Nueva ruta
+    context.arc(evt.x, evt.y, 10, 0, 2 * Math.PI); // Dibuja un círculo completo
+    context.strokeStyle = 'green'; // Establece el color del contorno
+    context.lineCap='round';
+    context.lineJoin='round';
+    context.stroke(); // Dibuja el contorno del círculo
+    context.fillStyle = 'green'; // Establece el color de relleno
+    context.fill(); // Rellena el círculo
 }
+
+canvas.addEventListener('click', draw);
